ea3707a7e451df83c5257136ce8d62b1226aecb2
lkatkus <34283585+lkatkus@users.noreply.github.com>
2018-03-23 14:11:52 +0200

    Basic functionality for stage builder

---
+++ Stage builder/js/script.js
@@ -0,0 +1,110 @@
+
+var builderLayout; /* lAYOUT GENERATION FORM */
+var tileTypeSelectors; /* TYLE TYPE BUTTON CONTAINER */
+var builderLayoutContainer; /* CONTAINER FOR APPENDING GENERATED LAYOUT */
+
+var newTileType; /* PLACEHOLDER FOR KEEPING SELECTED TYLE TYPE */
+var levelArray = []; /* PLACEHOLDER FOR LEVEL LAYOUT ARRAY */
+
+var numberOfTileTypes = 15; /* NUMBER OF POSIBLE TILE TYPES FOR GENERATING TILE BUTTONS */
+
+window.addEventListener('load', function(){
+
+    newTileType = 'red';
+
+    // SELECTORS
+    builderLayout = document.getElementById('builderLayout');
+    tileTypeSelectors = document.getElementById('tileTypeSelectors');
+    builderLayoutContainer = document.getElementById('builderLayoutContainer');
+
+    // LAYOUT GENERATION
+    builderLayout.addEventListener('submit', function(event){
+        // PREVENT FORM SUBMIT
+        event.preventDefault();
+
+        // CAPTURE FORM ELEMENTS
+        let formElements = document.getElementById("builderLayout").elements;
+        let postData = {}; /* OBJECT FOR STORING FORM DATA */
+
+        // PUSH FORM DATA TO PLACEHOLDER OBJECT
+        for (let i = 0; i < formElements.length; i++){
+            if (formElements[i].type != "submit"){
+                postData[formElements[i].name] = formElements[i].value;
+            };
+        };
+
+        // CREATE TILES AND APPEND TO CONTAINER
+        for(let i = 0; i < postData.layoutRows; i++){
+            // CREATE ROW DIV
+            let rowDiv = document.createElement('div');
+            rowDiv.classList.add('layoutRow');
+
+            for(let j = 0; j < postData.layoutCols; j++){
+                // CREATE TILE DIV
+                let tileDiv = document.createElement('div');
+                tileDiv.classList.add('layoutTile');
+                tileDiv.setAttribute('value', 0);
+
+                // APPEND TILE DIV TO ROW DIV
+                rowDiv.appendChild(tileDiv);
+            };
+
+            // APPEND ROW DIV
+            builderLayoutContainer.appendChild(rowDiv);
+        }
+
+    });
+    // END LAYOUT GENERATION
+
+    // GET NEW TILE TYPE
+    builderLayoutContainer.addEventListener('mousedown', function(){
+        // SELECT TILE WHICH WAS CLICKED
+        let tile = document.elementFromPoint(event.clientX, event.clientY);
+
+        // CHECK IF TILE WAS CLICKED
+        if(tile.classList.contains('layoutTile')){
+            if(newTileType != 0){
+                // APPLY VALUE
+                tile.style.backgroundImage  = `url(\"images/tiles/tile-${newTileType}.png\")`;
+                tile.setAttribute('value', newTileType);
+            }else if(newTileType == 0){
+                tile.style.backgroundImage  = ``;
+                tile.setAttribute('value', newTileType);
+            }
+        };
+    });
+    // END GET NEW TILE TYPE
+
+    // APPLY NEW TYPE TO LAYOUT
+    tileTypeSelectors.addEventListener('mousedown', function(){
+        // SELECT TYPE WHICH WAS CLICKED
+        let type = document.elementFromPoint(event.clientX, event.clientY);
+
+        // APPLY TYPE TO PLACEHOLDER
+        newTileType = type.getAttribute('value');
+    });
+    // END APPLY NEW TYPE TO LAYOUT
+})
+
+// GENERATE LEVEl ARRAY
+function generateLevelArray(){
+    // SELECT ALL ROWS IN LAYOUT
+    let rows = document.getElementById("builderLayoutContainer").querySelectorAll('.layoutRow');
+
+    // GET ALL TILE DIV VALUES AND PUSH TO LEVEL ARRAY
+    for(let i = 0; i < rows.length; i++){
+        // CREATE SINGLE ROW ARRAY
+        let rowArray = [];
+
+        // SELECT ALL TILES ON A SINGLE ROW
+        let tiles  = rows[i].querySelectorAll('.layoutTile');
+
+        for(let j = 0; j < tiles.length; j++){
+            // PUSH TILE DIV VALUE TO ROW ARRAY
+            rowArray.push(Number(tiles[j].getAttribute('value')));
+        };
+
+        // PUSH ROW ARRAY TO LEVEL ARRAY
+        levelArray.push(rowArray);
+    };
+}
