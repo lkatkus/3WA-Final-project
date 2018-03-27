dfb64e6faa022d954b387a6188e3703738738dc2
Laimonas Katkus <laimonas@katkus.eu>
2018-03-27 20:47:12 +0300

    Added quote block

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,15 @@
         </div>
     </div>
 
+    <div class="row no-gutters">
+        <div class="col-12  bg-warning  py-5">
+            <blockquote class="blockquote text-center">
+                <p class="mb-0">Stop calling me fat! I have big bones.</p>
+                <footer class="blockquote-footer">Phat The Cat</footer>
+            </blockquote>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-12">
             <h2 class="text-center bg-warning text-uppercase">Featured</h2>
@@ -21,8 +30,29 @@
                     @endforeach
                 <div class="col-4 d-flex justify-content-center align-items-center bg-warning">
                     <div class="">
-                        Read more
+                        Load more
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row no-gutters">
+        <div class="col-12  bg-primary py-4 text-center">
+            <h2 class="text-uppercase">Currently</h2>
+            <br>
+            <div class="row no-gutters">
+                <div class="col-4 vr">
+                    <h3>Users</h3>
+                    <p>38</p>
+                </div>
+                <div class="col-4 vl vr">
+                    <h3>Levels</h3>
+                    <p>53</p>
+                </div>
+                <div class="col-4 vl">
+                    <h3>Play sessions</h3>
+                    <p>245</p>
                 </div>
             </div>
         </div>
@@ -38,12 +68,13 @@
                 @endforeach
             <div class="col-4 d-flex justify-content-center align-items-center bg-danger">
                 <div class="">
-                    Read more
+                    Load more
                 </div>
             </div>
             </div>
         </div>
     </div>
+
 
 </div>
 @endsection
